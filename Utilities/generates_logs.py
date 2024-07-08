fdd191e821c5eb6f7290900c011315506c63b58e
piyush dravyakar <piyushdravyakar69@gmail.com>
2024-07-08 10:43:14 +0530

    ci

--- Utilities/generates_logs.py
+++ Utilities/generates_logs.py
@@ -1,15 +1,15 @@
-import logging
-
-class LogGen:
-
-    @staticmethod
-    def logger():
-        logg = logging.getLogger(__name__)
-        # filehandle = logging.FileHandler("/pythonProject/Logs/generate.log")
-        # formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        #
-        # filehandle.setFormatter(formatter)
-        # logg.addHandler(filehandle)
-        logg.setLevel(logging.INFO)
-        logg.setLevel(logging.DEBUG)
-        return logg
+# import logging
+#
+# class LogGen:
+#
+#     @staticmethod
+#     def logger():
+#         logg = logging.getLogger(__name__)
+#         # filehandle = logging.FileHandler("/pythonProject/Logs/generate.log")
+#         # formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+#         #
+#         # filehandle.setFormatter(formatter)
+#         # logg.addHandler(filehandle)
+#         logg.setLevel(logging.INFO)
+#         logg.setLevel(logging.DEBUG)
+#         return logg
