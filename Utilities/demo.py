e3dc1fd714a8932b1d10f31fcb3b76a8b72a6427
Piyush <piyushdravyakar48@gmail.com>
2024-07-08 21:36:02 +0530

    adding demo file

--- Utilities/demo.py
+++ Utilities/demo.py
@@ -5,6 +5,7 @@ driver = webdriver.Chrome()
 driver.get("https://www.makemytrip.com/hotels/")
 driver.implicitly_wait(10)
 driver.maximize_window()
+print("pa")
 
 driver.find_element(By.XPATH, "//span[text()='Price per Night']").click()
 driver.find_element(By.XPATH, "//li[text()='₹5000+']").click()
