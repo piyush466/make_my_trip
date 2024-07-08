8a56ec5def47c39392252030f71fa33bf789a47c
Piyush <piyushdravyakar48@gmail.com>
2024-07-08 21:29:46 +0530

    adding demo file

--- Utilities/demo.py
+++ Utilities/demo.py
@@ -1,4 +1,12 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-print(os.getenv("EMAIL"))
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.makemytrip.com/hotels/")
+driver.implicitly_wait(10)
+driver.maximize_window()
+
+driver.find_element(By.XPATH, "//span[text()='Price per Night']").click()
+driver.find_element(By.XPATH, "//li[text()='₹5000+']").click()
+
+driver.quit()
