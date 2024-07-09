90078ed280d4d5b4e322cbd101967719b48905bd
Piyush <piyushdravyakar48@gmail.com>
2024-07-09 21:51:16 +0530

    add

--- Utilities/demo.py
+++ Utilities/demo.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -9,5 +11,6 @@ print("pa")
 
 driver.find_element(By.XPATH, "//span[text()='Price per Night']").click()
 driver.find_element(By.XPATH, "//li[text()='₹5000+']").click()
-
+time.sleep(6)
 driver.quit()
+print("pass")
